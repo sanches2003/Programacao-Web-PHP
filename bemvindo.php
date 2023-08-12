94b07a05219009aa21bab6be2c713fd0cd835be7
Felipe Sanches <felipesteam2010@gmail.com>
2023-08-11 21:43:05 -0300

    Delete bemvindo.php

--- bemvindo.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $nome = $_POST["nome"];
-    echo "Konnichiwa $nome <br>";
-    $valor1 = $_POST["valor1"];
-    $valor2 = $_POST["valor2"];
-    echo "O total da soma é: ", $valor1+$valor2;
