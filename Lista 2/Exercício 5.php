8a4f2b8fee6c4f063634615488d21defe1a0db9d
Felipe Sanches <felipesteam2010@gmail.com>
2023-08-20 21:12:17 -0300

    Create Exercício 5.php

---
+++ Lista 2/Exercício 5.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <!-- Meta tags Obrigatórias -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <title>Lista 2 - Exercício 5</title>
+  </head>
+  <body class="container mt-3">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <h1>
+    <h3>Crie um algoritmo que solicite um número, faça o cálculo fatorial e exiba o resultado na tela.</h3>
+    <form action="Exercício 5_resp.php" method="POST">
+    <br>
+      <div class=container>  
+        <div class="row">
+            <div class="col">
+                <label for="valor">Digite um número para calcular seu fatorial</label>
+                <input type="number" name="valor" id="valor" class="form-control">
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div  class="col"> 
+                <button type="submit" class="btn btn-danger">Submeter</button>
+            </div>
+        </div>
+    </form>
+  </body>
+</html>
