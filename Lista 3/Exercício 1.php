5467fb10b65b7a49d848b671ad7b637bb9c08b53
Felipe Sanches <felipesteam2010@gmail.com>
2023-08-27 12:43:23 -0300

    Update Exercício 1.php

--- Lista 3/Exercício 1.php
+++ Lista 3/Exercício 1.php
@@ -14,7 +14,12 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-        <form action="Exercício 1.php" method="POST">
+        <form action="Exercício 1_resp.php" method="POST">
+        <h4>Entre com os dados de 10 alunos de uma classe, recebendo as informações como
+nome e uma nota do aluno. Armazene estes dados em um mapa ordenado. Ao
+final do programa mostrar a média de nota da classe, e o nome do aluno que
+obteve maior nota.
+</h4>
             <?php for($i=1; $i<=3; $i++){?>           
             <div class="row">
                 <div class="col">
@@ -29,19 +34,9 @@
             <?php } ?>
             <div class="row">
                 <div class="col mt-2 mb-2">
-                    <button type="submit" class="btn btn-info">Calcular</button>
+                    <button type="submit" class="btn btn-info">Submeter</button>
                 </div>
             </div>
         </form>
-        <?php
-            if ($_POST){
-                for($i=1; $i<=3; $i++){
-                    $vetor[$_POST["aluno$i"]] = $_POST["nota$i"];
-                }
-                foreach($vetor as $chave => $valor){
-                    echo "<br> Nome do aluno: $chave - Nota: $valor";
-                }
-            }
-        ?>
 </body>
 </html>
