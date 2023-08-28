f0e9c771544f7cd13a21355d07e0d08524e6272f
Felipe Sanches <felipesteam2010@gmail.com>
2023-08-28 20:29:38 -0300

    Update Exercício 2.php

--- Lista 3/Exercício 2.php
+++ Lista 3/Exercício 2.php
@@ -7,9 +7,21 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
   </head>
   <body class="container">
-    <h1>Exercício</h1>
-    <form action="Exercício 3_resp.php" method="POST">
-
+    <h1>Exercício 2 - Lista 3</h1>
+    <form action="Exercício 2_resp.php" method="POST">
+    <div class="row">
+            <?php for($i=1;$i<=5;$i++) { ?>
+                <div class="col">
+                    <label class="form-label" for="v<?=$i?>">Informe o valor <?=$i?>, sendo ele, positivo, negativo, par ou ímpar:</label>
+                    <input type="number" id="v<?=$i?>" name="v<?=$i?>" class="form-control"/>
+                </div>
+            <?php } ?>
+        </div>
+        <div class="row">
+            <div class="col">
+                <button type="submit" class="btn btn-success">Verificar</button>
+            </div>
+        </div>
     </form>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
   </body>
