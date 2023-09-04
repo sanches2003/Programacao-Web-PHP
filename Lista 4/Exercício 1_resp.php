5521a4c19ff7ee1fff4175f9076de514d9f20950
Felipe Sanches <felipesteam2010@gmail.com>
2023-09-04 20:34:32 -0300

    Update Exercício 1_resp.php

--- Lista 4/Exercício 1_resp.php
+++ Lista 4/Exercício 1_resp.php
@@ -8,7 +8,13 @@
   </head>
   <body class="container">
     <?php 
-    
+    $dia= $_POST["dia"];
+    $mes= $_POST["mes"];
+    $ano= $_POST["ano"];
+    if (checkdate($mes, $dia, $ano))
+    echo"Válido";
+    else
+    echo"Inválido";
     ?>
    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
