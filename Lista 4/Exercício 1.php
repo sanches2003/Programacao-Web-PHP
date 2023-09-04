0d6fa362d44deb0b602bf9cdc323250fe4f53617
Felipe Sanches <felipesteam2010@gmail.com>
2023-09-04 20:15:58 -0300

    Create Exercício 1.php

---
+++ Lista 4/Exercício 1.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Exercício 1 - Lista 4</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+  </head>
+  <body class="container">
+  <h3>Faça um programa que leia uma data e verifique se ela é válida. Além disso 
+    informe ao usuário seu respectivo dia da semana.</h3>
+    <form action="Exercício 1_resp.php" method="POST">
+    <h5>Digite uma data</h5>
+    <form method="POST" action="Exercício 1_resp.php">
+        <input type="date" name="date" id="date">
+        <input type="submit" value="Validar">
+    </form>
+    </div>
+    </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+  </body>
+</html>
