9bc43efc48309fe12f2cfa61fcf0fe8a5d667519
Felipe Sanches <felipesteam2010@gmail.com>
2023-09-04 21:32:39 -0300

    Create Exercício 2_resp.php

---
+++ Lista 4/Exercício 2_resp.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Exercício 2 - Lista 4</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+  </head>
+  <body class="container">
+    <?php 
+        for($i=1;$i<=10;$i++){
+                $vetor[$i] = $_POST["$i"];
+            }
+            $nomes = $_POST['nomes'];
+            sort($nomes);
+    ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+  </body>
+</html>
