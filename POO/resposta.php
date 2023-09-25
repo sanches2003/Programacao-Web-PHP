086267b90c24ea95cb110e3694b87836b123e784
Felipe Sanches <felipesteam2010@gmail.com>
2023-09-25 19:59:59 -0300

    Update resposta.php

--- POO/resposta.php
+++ POO/resposta.php
@@ -1,10 +1,10 @@
 <?php
 
     include("Pessoa.php");
-    $obj =  new Pessoa();
-    $obj->setNome($_POST['nome']);
-    $obj->setEndereco($_POST['endereco']);
-    $obj->setIdade($_POST['idade']);
+    $obj =  new Pessoa($_POST['nome'], $_POST['endereco'], $_POST['idade']);
+    #$obj->setNome($_POST['nome']);
+    #$obj->setEndereco($_POST['endereco']);
+    #$obj->setIdade($_POST['idade']);
 
     echo "Nome: {$obj->getNome()}<br/>";
     echo "Endereco: {$obj->getEndereco()}<br/>";
