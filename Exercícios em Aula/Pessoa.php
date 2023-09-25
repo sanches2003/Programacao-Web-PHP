783420c93d369f078f5644581793ab6e5f7989f0
Felipe Sanches <felipesteam2010@gmail.com>
2023-09-25 19:43:19 -0300

    Update Pessoa.php

--- Exercícios em Aula/Pessoa.php
+++ Exercícios em Aula/Pessoa.php
@@ -2,11 +2,38 @@
 <?php 
 
 
+class Pessoa {
 
-    function getEndereco()
-    {
+    private $nome;
+    private $endereco;
+    private $idade;
 
+    function setNome($nome){
+        $this->nome = $nome;
+    }
+
+    function getNome(){
+        return $this->nome;
+    }
+
+    function setEndereco($endereco){
+        $this->endereco = $endereco;
+    }
+
+    function getEndereco(){
         return $this->endereco;
     }
+
+    function setIdade($idade){
+        if ($idade>=0)
+            $this->idade = $idade;
+        else
+            $this->idade = 0;
+    }
+
+    function getIdade(){
+        return $this->idade;
+    }
+}
 
 ?>
