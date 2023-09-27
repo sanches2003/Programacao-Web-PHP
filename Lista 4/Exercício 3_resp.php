3d0e2325d0c5c89647e7583d52d6969c48975cff
Felipe Sanches <felipesteam2010@gmail.com>
2023-09-27 20:53:39 -0300

    Update Exercício 3_resp.php

--- Lista 4/Exercício 3_resp.php
+++ Lista 4/Exercício 3_resp.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>exe 3 resposta</title>
+    <title>Exercício  3 - Resposta</title>
     <!-- Adicione o link para o Bootstrap CSS (min version) -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
